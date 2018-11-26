8c734baafaf73fbb76437ac567e8d34603636656
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2018-11-26 09:18:22 +0100

    - Snippets DRF Project

---
+++ snippets/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import SnippetSerializer, UserSerializer
+from .models import Snippet
+
+
+# Create your views here.
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
