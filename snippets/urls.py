5695d7e43dc107b42a2077b4194ef6052c11b5cb
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2018-11-26 11:47:04 +0100

    - Snippets hyperlinking returning HTML

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,6 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlighted', views.SnippetHighlight.as_view(), name='snippet-highlight'),
     path('users/', views.UserList.as_view(), name='user-list'),
     path('snippets/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
     path('', views.api_root)
--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.reverse import reverse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,6 +14,15 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format)
     })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer, )
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 # Create your views here.
